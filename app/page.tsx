e332008972781afcd9ca4bf0760b0a0d7b7b8477
briotechwebcrafthub <briotechwebcraft@gmail.com>
2025-04-23 19:43:53 +0530

    added rpojcts

--- app/page.tsx
+++ app/page.tsx
@@ -420,15 +420,7 @@ export default function Home() {
 
       <footer className="bg-[#fafafa] py-10  px-6 md:px-0 md:mx-auto border-t">
         <div className="flex flex-col  justify-between gap-y-3 xl:w-4/5 2xl:w-[68%] mx-auto">
-          <h1 className="text-3xl md:text-5xl font-medium ">
-            <Image
-              src={"/logo/logoo.png"}
-              width={10000}
-              height={10000}
-              className="w-40"
-              alt="image"
-            />{" "}
-          </h1>
+         
           <p className="text-left  text-lg  text-gray-500">+91 7591902596</p>
           <p className="text-left  text-lg  text-gray-500">
             fedgixtechnologies@gmail.com
